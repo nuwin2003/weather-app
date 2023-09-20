4b04930c4f35dec5201274f0cef4cf0ae5d6e22f
Nuwin Vinwath <131539550+nuwin2003@users.noreply.github.com>
2023-09-20 11:47:24 +0530

    Nav bar and Search bar added

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const searchBox = document.getElementById('searchText');
+const btnSearch = document.getElementById('btnSearch');
+
+btnSearch.addEventListener('click', e=>{
+    console.log(searchBox.value);
+});
