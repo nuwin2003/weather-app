347761f7274efcabfec60f3bdd0c34ad87a31161
Nuwin Vinwath <131539550+nuwin2003@users.noreply.github.com>
2023-09-21 23:34:48 +0530

    Page maps linked and navbar edited

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const searchBox = document.getElementById('searchText');
 const btnSearch = document.getElementById('btnSearch');
 const alertWrongName = document.getElementById('alertWrongName');
+const locationOnNavbar = document.getElementById('locationOnNavbar');
 
 btnSearch.addEventListener('click', e=>{
     console.log(searchBox.value);
@@ -8,7 +9,10 @@ btnSearch.addEventListener('click', e=>{
         alertWrongName.style.display = "block";
     }else{
         alertWrongName.style.display = "none";
+        locationOnNavbar.innerText = searchBox.value;
     }
 
+
+    
     searchBox.value = "";
 });
