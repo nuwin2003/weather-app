5c4bb515e620376ee8ebcc83ad46bafb5239b83d
Nuwin Vinwath <131539550+nuwin2003@users.noreply.github.com>
2023-09-21 14:02:14 +0530

    Alert added and checked

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,11 @@
 const searchBox = document.getElementById('searchText');
 const btnSearch = document.getElementById('btnSearch');
+const alertWrongName = document.getElementById('alertWrongName');
 
 btnSearch.addEventListener('click', e=>{
     console.log(searchBox.value);
+    if(searchBox.value == "Colombo"){
+        alertWrongName.style.display = "block";
+    }
     searchBox.value = "";
 });
